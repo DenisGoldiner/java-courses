943570f8927f76276b91fb56e53fa3c83d42011d
DenisGoldiner <goldiner19@gmail.com>
2015-08-26 01:33:08 +0300

    Calculate with mathods

--- Calculate.java
+++ Calculate.java
@@ -2,19 +2,20 @@ public class Calculate{
 
     public static void main (String[] args){
 
-        System.out.println("Calculate ...");
+        System.out.println("Calculate ... \n");
 
-	int first = Integer.valueOf(args[0]);
-	int second = Integer.valueOf(args[1]);
+	double first = Integer.valueOf(args[0]);
+	double second = Integer.valueOf(args[1]);
 
-	int summ = first + second;
-	int minus = first - second;
-	double multiplication = first * second;
-	double division = (double)first / (double)second;
-	double power = Math.pow(first, second);
+	
+	double summ = summ(first, second);
+	double minus = minus(first, second);
+	double multiplication = multiplication(first, second);
+	double division = division(first, second);
+	double power = power(first, second);
 
  
-	System.out.println("first          = " + first + ", second = " + second);
+	System.out.println("first = " + first + ", second = " + second + '\n');
 	System.out.println("Summ           = " + first + " + " + second + " = " + summ);
 	System.out.println("Minus          = " + first + " - " + second + " = " + minus);
 	System.out.println("Division       = " + first + " / " + second + " = " + division);
@@ -22,5 +23,42 @@ public class Calculate{
 	System.out.println("Power          = " + first + " ^ " + second + " = " + power);			
 
     }
+	
+	public static double summ (double x, double y){
+		double result;
+		result = x + y;
+		return result;
+	}
+	
+	public static double minus (double x, double y){
+		double result;
+		result = x - y;
+		return result;
+	}
+	
+	public static double division (double x, double y){
+		double result;
+		result = x / y;
+		return result;
+	}
+	
+	public static double multiplication (double x, double y){
+		double result;
+		result = x * y;
+		return result;
+	}
 
+	public static double power (double x, double y){
+		double result = 1;
+		if(y >= 0){
+			for(int i = 0; i < y; ++i){
+				result *= y;
+			}
+		} else if(y < 0){
+				for(int i = 0; i < -y; ++i){
+				result /= y;
+			}
+		}
+		return result;
+	}	
 }
