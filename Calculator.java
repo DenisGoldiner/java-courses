a071cdef3a8871e5c5086181a7a19ef22b0061e2
DenisGoldiner <goldiner19@gmail.com>
2015-08-30 16:36:39 +0300

    with methods

--- Calculator.java
+++ Calculator.java
@@ -1,14 +1,42 @@
 public class Calculator {
 	
-	private int result;
+	private double result;
+	private double timeResult;
 	
-	public void summ (int ... params){
-		for(Integer param : params){
-			this.result += param;
+	public void summ (int ... entery){
+		this.timeResult = 0;
+		for(Integer param : entery){
+			this.timeResult += param;
 		}
+		this.result = this.timeResult;
 	}
 	
-	public int getResult(){
+	public void substruct (int ... entery){
+		this.timeResult = entery[0] * 2;
+		for(Integer param : entery){
+			this.timeResult -= param;
+		}
+		this.result = this.timeResult;
+	}
+	
+	public void muliply (int ... entery){
+		this.timeResult = 1;
+		for(Integer param : entery){
+			this.timeResult *= param;
+		}
+		this.result = this.timeResult;
+	}
+	
+	public void division (int ... entery){
+		this.timeResult = (double)entery[0] * 2;
+		for(Integer param : entery){
+			this.timeResult /= (double)param;
+		}
+		this.result = this.timeResult;
+	}
+	
+	
+	public double getResult(){
 		return (this.result);
 	}
 	
--- InteractRunner.java
+++ InteractRunner.java
@@ -2,25 +2,63 @@ import java.util.Scanner;
 
 public class InteractRunner{
 	
-	public static void main(String[] args){
+	public static void main (String[] args){
 		Scanner reader = new Scanner(System.in);
+		
 		try{
 			Calculator calc = new Calculator();
 			String exit = "no";
+
 			while(!exit.equals("yes")){
+				
+				String operationExist = "yes";
+				
 				System.out.println("Enter first arg : ");
 				String first = reader.next();
 				System.out.println("Enter second arg : ");
 				String second = reader.next();
-				calc.summ(Integer.valueOf(first), Integer.valueOf(second));
-				System.out.println("Result = " + calc.getResult());
-				calc.cleanResult();
-				System.out.println("Exit yes/no ?");
+				
+				while(operationExist.equals("yes")){
+					operationExist = "no";
+					
+				System.out.println("\nWhat operation would you like to do? \n");
+				System.out.println("Write \"summ\" if operation you want is + ");
+				System.out.println("Write \"minus\" if operation you want is - ");
+				System.out.println("Write \"multiply\" if operation you want is * ");
+				System.out.println("Write \"division\" if operation you want is / \n");
+				
+				String operation = reader.next();
+					
+					switch(operation){
+						case "summ": 
+							calc.summ(Integer.valueOf(first), Integer.valueOf(second));
+							break;
+						case "minus": 
+							calc.substruct(Integer.valueOf(first), Integer.valueOf(second));
+							break;
+						case "multiply": 
+							calc.muliply(Integer.valueOf(first), Integer.valueOf(second));
+							break;
+						case "division": 
+							calc.division(Integer.valueOf(first), Integer.valueOf(second));
+							break;
+						default: 
+							System.out.println("There is no such command try again yes/no ?");
+							operationExist = reader.next();
+					}
+				}
+				
+				System.out.println("\nResult = " + calc.getResult());
+				calc.cleanResult();							
+				
+				System.out.println("\nExit yes/no ? \n");
 				exit = reader.next();
 			}
+			
 		} finally {
 			reader.close();
 		}
+		
 	}
 	
 }
